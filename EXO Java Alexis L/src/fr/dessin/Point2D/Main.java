3714324ccd3cbb7fe6b0f062fd80016b0ef41ac1
Alexis Larochette <58219897+ChercheurSwifer@users.noreply.github.com>
2021-10-04 12:09:05 +0200

    Update Main.java

--- EXO Java Alexis L/src/fr/dessin/Point2D/Main.java
+++ EXO Java Alexis L/src/fr/dessin/Point2D/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Point2D point2d = new Point2D();
+        point2d.translater(35, 40);
         point2d.afficher();
-        point2d.translater(point2d.getX(), point2d.getY());
     }
 }
